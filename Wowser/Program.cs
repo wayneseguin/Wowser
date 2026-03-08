d3dc8b7919fc7ee72d5e31ed53a37a4f37e0f56b
wayneseguin <wseguin@enformion.com>
2026-03-08 10:34:54 -0400

    Initial commit: Wowser standalone browser consuming Magra.CefDriver NuGet

---
+++ Wowser/Program.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Windows.Forms;
+using CefDriver;
+using Serilog;
+
+namespace Wowser
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .WriteTo.Console()
+                .WriteTo.File("wowser.log", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            var url = args.Length > 0 ? args[0] : "https://www.google.com";
+            Application.Run(new MainForm(url, logger));
+        }
+    }
+}
